3a0d3014b4ee07b7246749ef6244860c785358d1
Neal Haggard <neal.haggard@sas.com>
2022-11-08 10:36:06 -0500

    Setup Firebase Hosting & Deploy

--- apps/plc-plan/src/environments/environment.prod.ts
+++ apps/plc-plan/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyDS6LH9fIf5S5n9zGL5BDo5sqtQAfwqWqU",
+    authDomain: "plc-plan.firebaseapp.com",
+    projectId: "plc-plan",
+    storageBucket: "plc-plan.appspot.com",
+    messagingSenderId: "557619893305",
+    appId: "1:557619893305:web:9d7046eb5a97e6f96d69e8",
+    measurementId: "G-J6WMZ10H7P"
+  }
 };
