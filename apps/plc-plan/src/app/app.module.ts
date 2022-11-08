390eb65289347e1db60431a2b4c5ad7e441880fe
Neal Haggard <neal.haggard@sas.com>
2022-11-08 13:26:33 -0500

    NX generate @angular/fire:ng-add

--- apps/plc-plan/src/app/app.module.ts
+++ apps/plc-plan/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), providePerformance(() => getPerformance())],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/plc-plan/src/environments/environment.ts
+++ apps/plc-plan/src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDS6LH9fIf5S5n9zGL5BDo5sqtQAfwqWqU",
-    authDomain: "plc-plan.firebaseapp.com",
-    projectId: "plc-plan",
-    storageBucket: "plc-plan.appspot.com",
-    messagingSenderId: "557619893305",
-    appId: "1:557619893305:web:9d7046eb5a97e6f96d69e8",
-    measurementId: "G-J6WMZ10H7P"
+    projectId: 'plc-plan',
+    appId: '1:557619893305:web:9d7046eb5a97e6f96d69e8',
+    databaseURL: 'https://plc-plan-default-rtdb.firebaseio.com',
+    storageBucket: 'plc-plan.appspot.com',
+    apiKey: 'AIzaSyDS6LH9fIf5S5n9zGL5BDo5sqtQAfwqWqU',
+    authDomain: 'plc-plan.firebaseapp.com',
+    messagingSenderId: '557619893305',
+    measurementId: 'G-J6WMZ10H7P',
   }
 };
 
