0ca9222bf6716625d7c86903466818b6ab0de6ba
Neal Haggard <neal.haggard@sas.com>
2022-11-09 11:56:49 -0500

    Add Button & Icons to main menu

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,11 +2,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainMenuComponent } from './main-menu/main-menu.component';
-import { AppBarModule } from "@syncfusion/ej2-angular-navigations";
+import { AppBarModule } from '@syncfusion/ej2-angular-navigations';
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
         
 @NgModule({
   imports: [
     AppBarModule,
+    ButtonModule,
     CommonModule,
   ],
   declarations: [
